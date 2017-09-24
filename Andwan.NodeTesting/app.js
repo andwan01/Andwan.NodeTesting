19c036c7c00d84f56ca2f9ad52263f648a8ca684
Andrew Wan <andwan@gmail.com>
2017-09-24 23:31:47 +0100

    First project files

--- Andwan.NodeTesting/app.js
+++ Andwan.NodeTesting/app.js
@@ -1,3 +1,10 @@
-'use strict';
+"use strict";
 
-console.log('Hello world');
+var User = require("./user");
+
+var u1 = new User("Andrew","Wan");
+
+console.log(typeof u1);
+console.log(u1.fullName);
+console.log(u1.getDateCreated());
+
---
+++ Andwan.NodeTesting/user.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+var User = class {
+    constructor(firstName, lastName) {
+        var _firstName = firstName;
+        var _lastName = lastName;
+        var _dateCreated = new Date();
+
+        this.getFirstName = function () { return _firstName; }
+        this.getLastName = function () { return _lastName; }
+        this.getDateCreated = function() { return _dateCreated; }
+    }
+
+    get fullName() {
+        return this.getFirstName().concat(" ", this.getLastName());
+    }
+};
+
+module.exports = User;
